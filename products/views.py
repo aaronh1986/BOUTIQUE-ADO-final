1a7f651709faf24d9239fae39e553f73d1953120
aaronh1986 <aaronharbourne@gmail.com>
2021-09-10 17:26:01 +0000

    sksks

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 def all_products(request):
-    """ A view show all products including searching and sorting """
+    """ A view to show all products including searching and sorting """
     products = Product.objects.all()
 
     context = {
@@ -11,3 +11,14 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ A view to show product details """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
