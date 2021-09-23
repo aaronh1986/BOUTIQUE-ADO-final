aeaca9e15797a58474315343449321b8df9517e8
aaronh1986 <aaronharbourne@gmail.com>
2021-09-23 22:19:54 +0000

    sss

--- checkout/models.py
+++ checkout/models.py
@@ -25,10 +25,9 @@ class Order(models.Model):
 
     def _generate_order_number(self):
         """
-        Generate a unique, random order no. using uuid
+        Generate a random, unique order number using UUID
         """
         return uuid.uuid4().hex.upper()
-
 
     def update_total(self):
         """
@@ -43,15 +42,14 @@ class Order(models.Model):
         self.grand_total = self.order_total + self.delivery_cost
         self.save()
 
-
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
-        and update the order total.
+        Override the original save method to set the order number
+        if it hasn't been set already.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
-
 
     def __str__(self):
         return self.order_number
@@ -64,7 +62,6 @@ class OrderLineItem(models.Model):
     quantity = models.IntegerField(null=False, blank=False, default=0)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
-
     def save(self, *args, **kwargs):
         """
         Override the original save method to set the lineitem total
@@ -72,7 +69,6 @@ class OrderLineItem(models.Model):
         """
         self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
-
 
     def __str__(self):
         return f'SKU {self.product.sku} on order {self.order.order_number}'
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
@@ -7,10 +7,9 @@ from .forms import OrderForm
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
-        messages.error(request, "Bag empty for now")
+        messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-    
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
