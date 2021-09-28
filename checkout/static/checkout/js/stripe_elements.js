23d471fe7b49a55a0700400b5e9f11eb5b9b2c2e
aaronh1986 <aaronharbourne@gmail.com>
2021-09-28 13:01:21 +0000

    sss

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -7,6 +7,7 @@
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
+console.log(clientSecret)
 var stripe = Stripe(stripePublicKey);
 var elements = stripe.elements();
 var style = {
@@ -72,3 +73,4 @@ form.addEventListener('submit', function(ev) {
         }
     });
 });
+
