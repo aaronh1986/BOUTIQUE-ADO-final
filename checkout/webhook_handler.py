c8a6841f75561a5cbf2e7a7eb8777c1463497023
aaronh1986 <aaronharbourne@gmail.com>
2021-09-28 20:02:28 +0000

    sss

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 
+from .models import Order, OrderLineItem
+from products.models import Product
+
+import json
+import time
 
 class StripeWH_Handler:
     """Handle Stripe webhooks"""
@@ -26,7 +31,7 @@ class StripeWH_Handler:
 
         billing_details = intent.charges.data[0].billing_details
         shipping_details = intent.shipping
-        grand_total = round(intent.data.charges[0].amount / 100, 2)
+        grand_total = round(intent.charges.data[0].amount / 100, 2)
 
         # Clean data in the shipping field
         for field, value in shipping_details.address.items():
